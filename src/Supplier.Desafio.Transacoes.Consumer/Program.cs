d3aeaac63d090770cadae2768ffc1174bc37d268
Igor Daflon do Couto <igor.couto@softplan.com.br>
2025-03-07 10:19:24 -0300

    Adiciona testes

--- src/Supplier.Desafio.Transacoes.Consumer/Program.cs
+++ src/Supplier.Desafio.Transacoes.Consumer/Program.cs
@@ -11,13 +11,18 @@ using Supplier.Desafio.Transacoes.Infra.Transacoes.Repositorios;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
 string connectionString = builder.Configuration.GetConnectionString("DefaultConnection")!;
-builder.Services.AddSingleton(new DapperDbContext("Server=172.30.94.105;Port=3307;Database=supplier_desafio;Uid=usuario;Pwd=senha;"));
+builder.Services.AddSingleton(new DapperDbContext(connectionString));
 
 builder.Services.AddScoped<ITransacoesServico, TransacoesServico>();
 builder.Services.AddScoped<ITransacoesRepositorio, TransacoesRepositorio>();
 
-builder.Services.AddMessageBus("host=localhost:5672;timeout=10")
+var messageBusConfig = builder.Configuration.GetSection("MessageBus");
+string messageBusConnection = messageBusConfig["Connection"]!;
+
+builder.Services.AddMessageBus(messageBusConnection)
     .AddHostedService<ResultadoTransacaoHandler>();
 
 var host = builder.Build();
